078cb1dac3c893330e86dc6ce83a0f080fc52b17
Sibusiso <itsbudamain.05@gmail.com>
2020-07-14 00:13:34 +0200

    Implemented collage effect with React Refs and CSS

--- src/Components/ImageList.js
+++ src/Components/ImageList.js
@@ -1,14 +1,16 @@
+import './ImageList.css'
 import React from 'react'
+import ImageCard from './ImageCard' 
 
 const ImageList = props => {
-	const images = props.images.map(({description, id, urls}) => {
+	const images = props.images.map((image) => {
 		return (
-			<img alt={description} key={id} src={urls.regular}/>
+			<ImageCard key={image.id} image={image} ImageCard/>
 		)
 	})
 
 	return(
-		<div>{images}</div>
+		<div className="image-list">{images}</div>
 	);
 }
 
